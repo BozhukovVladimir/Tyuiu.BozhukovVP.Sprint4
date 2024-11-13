65cf79a4d0bf97f9f961879c89732a982e1e1d91
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-11-13 16:46:53 +0500

    Task5

---
+++ Tyuiu.BozhukovVP.Sprint4.Task5.V25.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+using Tyuiu.BozhukovVP.Sprint4.Task5.V25.Lib;
+namespace Tyuiu.BozhukovVP.Sprint4.Task5.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] array ={{ -4, 3, -2, 2, 2, },
+
+                            { 3, -1, 1, -1, 3, },
+
+                            { 3, -2, 2, 2, -3, },
+
+                            { -4, -3, -2, -1, -1, },
+
+                            { 1, 2, 2, 3, 3, } };
+            int wail = 32;
+            int res = ds.Calculate(array);
+            Assert.AreEqual(wail, res);
+        }
+    }
+}
---
+++ Tyuiu.BozhukovVP.Sprint4.Task5.V25/Program.cs
@@ -0,0 +1,64 @@
+﻿using Tyuiu.BozhukovVP.Sprint4.Task5.V25.Lib;
+namespace Tyuiu.BozhukovVP.Sprint4.Task5.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random random = new Random();
+
+            Console.Title = "Спринт #4 | Выполнил Божуков В. П. | ИБКСб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы                                                 *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Божуков Владимир Павлович   | ИБКСб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов,                    *");
+            Console.WriteLine("* заполненный случайными значениями в диапазоне от -4 до 3.               *");
+            Console.WriteLine("* Найдите сумме чисел больше 0                                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите колличество строк");
+            int str = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите колличество столбцов");
+            int stl = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array = new int[str, stl];
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < str; i++)
+            {
+                for (int j = 0; j < stl; j++)
+                {
+
+                    array[i, j] = random.Next(-4, 3);
+                }
+            }
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < str; ++i)
+            {
+                for (int j = 0; j < stl; j++)
+                {
+                    Console.Write($"{array[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int z = ds.Calculate(array);
+            Console.WriteLine(z);
+
+            Console.ReadKey();
+        }
+    }
+}
