17ffad21bd61e4f420e1cc3782f57aeea13c3d2e
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-11-13 15:54:48 +0500

    Task2

---
+++ Tyuiu.BozhukovVP.Sprint4.Task2.V22/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.BozhukovVP.Sprint4.Task2.V22.Lib;
+namespace Tyuiu.BozhukovVP.Sprint4.Task2.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Божуков В. П. | ИБКСб-24-1 ";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                   *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Божуков Владимир Павлович | ИБКСб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 13 элементов заполненный         *");
+            Console.WriteLine("* случайными в диапазоне от 1 до 6 подсчитать произведение нечетных       *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int len;
+            Console.WriteLine("Введите количество элементов массива:");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[len];
+
+
+            for (int i = 0; i < len; i++)
+            {
+                array[i] = rnd.Next(1, 7);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Получившийся массив:");
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение нечетных элементов массива = " + ds.Calculate(array));
+            Console.ReadKey();
+        }
+    }
+}
